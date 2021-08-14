4af98b47e16ea2a4a534b037b6bbe56397c501b4
hava12 <hava12@naver.com>
2021-08-14 18:31:48 +0900

    업무 도우미 프로젝트 2021-08-14 Commit and Push 1. Thymeleaf 공부 및 적용

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,18 +1,36 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 @Controller
 public class MainController {
 
-    @GetMapping({"/","/main"})
-    public String main(){
+    @GetMapping({"/", "/main"})
+    public String main() {
         return "main";
     }
 
     @GetMapping("/format")
-    public String format(){
+    public String format(Model model) {
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("key", "1key");
+        map1.put("val", "1val");
+        Map<String, String> map2 = new HashMap<>();
+        map1.put("key", "2key");
+        map1.put("val", "2val");
+        Map<String, String> map3 = new HashMap<>();
+        map1.put("key", "3key");
+        map1.put("val", "3val");
+
+
+        Optional<Stream<Map<String, String>>> stream = Optional.empty();
+
+        model.addAttribute("");
         return "form/format";
     }
 
